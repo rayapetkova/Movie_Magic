b3add3cb012efbd532a430a7966502373f5797d6
Raya Petkova <rayapetkova2007@abv.bg>
2025-09-30 23:26:15 +0300

    Add express server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('working...')
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'))
