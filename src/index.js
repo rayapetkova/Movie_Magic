7e122b4bdb55449ed7e6f55c147d147a7c4e85bb
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-06 09:02:09 +0300

    Static file configuration

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
 
+app.use(express.static('src/public'));
+
 app.get('/', (req, res) => {
     res.render('home', { layout: false });
 });
