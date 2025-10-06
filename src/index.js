83b6dd9396f01cffcda783db5cc4f2caaa8693fe
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-06 08:55:40 +0300

    Express handlebars configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express'
+import handlebars from 'express-handlebars'
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.get('/', (req, res) => {
-    res.send('working...')
+    res.render('home', { layout: false });
 });
 
-app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'))
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
