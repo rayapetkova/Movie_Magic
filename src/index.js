12cfe6f7952a5cb9dc575a18ab1aae3f998b7253
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-06 20:51:12 +0300

    Extract routes in routes.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
-import handlebars from 'express-handlebars'
-import homeController from './controllers/homeController.js';
-import movieController from './controllers/movieController.js';
+import handlebars from 'express-handlebars';
+import routes from './routes.js';
 
 const app = express();
 
@@ -14,7 +13,6 @@ app.set('views', 'src/views');
 
 app.use(express.static('src/public'));
 
-app.use(homeController);
-app.use(movieController);
+app.use(routes);
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
