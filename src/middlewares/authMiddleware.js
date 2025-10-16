fbc30cacdf2efbe1be6152458952b806edaaee38
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-16 11:30:30 +0300

    Add user to req

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,20 +3,18 @@ import jwt from 'jsonwebtoken';
 export default function authMiddleware(req, res, next) {
     const token = req.cookies['auth'];
 
-    console.log('working');
-
     if (!token) {
-        console.log('no token');
         return next();
     }
 
     try {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        console.log('valid user');
+
+        req.user = decodedToken;
+        req.isAuthenticated = true;
 
         next();
     } catch {
-        console.log('invalid user');
         res.clearCookie('auth');
         res.redirect('/auth/login');
     }
