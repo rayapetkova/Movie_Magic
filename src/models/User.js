71c630e9937a1477bff3113ce32777aeadb0f1e2
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-22 01:02:23 +0300

    Add email and password validations in User model

--- src/models/User.js
+++ src/models/User.js
@@ -6,11 +6,15 @@ const userSchema = new Schema({
     email: {
         type: String,
         required: [true, 'Email is required.'],
-        unique: [true, 'Email already in use.']
+        unique: [true, 'Email already in use.'],
+        minLength: [10, 'Email must be at least 10 characters long.'],
+        match: [/@[a-zA-z]+\.[a-zA-Z]+$/, 'Email is not in valid format.']
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minLength: [6, 'Password must be at least 6 characters long.'],
+        match: [/^[a-zA-z0-9]+$/, 'Password must contain only letters and digits.']
     },
 });
 
