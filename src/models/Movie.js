297a9117c1e9858452f19c76a84927ad4d61b977
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-21 23:04:36 +0300

    Add Movie model validators

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,17 +1,58 @@
 import { Schema, Types, model } from "mongoose";
 
 const movieSchema = new Schema({
-    title: String,
-    category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    title: {
+        type: String,
+        required: [true, 'Title is required.'],
+        minLength: [5, 'Title must be at least 5 characters long.'],
+        match: [/^[a-zA-z0-9 ]+$/, 'Title should consist of only letters, digits and whitespaces.']
+    },
+    category: {
+        type: String,
+        required: [true, 'Category is required'],
+        enum: {
+            values: ['tv-show', 'animation', 'movie', 'documentary', 'short-film'],
+            message: 'Category invalid'
+        }
+    },
+    genre: {
+        type: String,
+        required: [true, 'Genre is required.'],
+        minLength: [5, 'Genre must be at least 5 chracters long.'],
+        match: [/^[a-zA-z0-9 ]+$/, 'Genre should consist of only letters, digits and whitespaces.']
+    },
+    director: {
+        type: String,
+        required: [true, 'Director is required.'],
+        minLength: [5, 'Director must be at least 5 characters long.'],
+        match: [/^[a-zA-z0-9 ]+$/, 'Director should consist of only letters, digits and whitespaces.']
+    },
+    year: {
+        type: Number,
+        required: [true, 'Year is required.'],
+        min: [1990, 'Year cannot be less than 1990.'],
+        max: [2024, 'Year cannot be greater than 2024.']
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image URL is required.'],
+        match: [/^https?:\/\//, 'Image URL has invlid format.']
+    },
+    rating: {
+        type: Number,
+        required: [true, 'Rating is required.'],
+        min: [1, 'Rating cannot be less than 1.'],
+        max: [5, 'Rating cannot be greater than 5.']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required.'],
+        minLength: [20, 'Description must be at lest 20 characters long.'],
+        match: [/^[a-zA-z0-9 ]+$/, 'Description should consist of only letters, digits and whitespaces.']
+    },
     casts: [{
         type: Types.ObjectId,
-        ref: 'Cast'
+        ref: ' Cast'
     }],
     creator: {
         type: Types.ObjectId,
