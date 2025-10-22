47a55de46ebfb329d658557850b966a59a6226cf
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-22 15:07:27 +0300

    Display error messages on cast create and add data persistence even if an error is shown

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import castService from "../services/castService.js";
 import { isAuth } from "../middlewares/authMiddleware.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
 
 const castController = Router();
 
@@ -11,9 +12,16 @@ castController.get('/create', isAuth, (req, res) => {
 castController.post('/create', isAuth, async (req, res) => {
     const castData = req.body;
 
-    castService.create(castData);
+    try {
+        await castService.create(castData);
 
-    res.redirect('/');
+        res.redirect('/');
+    } catch (err) {
+        const errorMessage = getErrorMessage(err);
+
+        res.status(400).render('casts/create', { error: errorMessage, cast: castData });
+    }
+
 });
 
 export default castController;
