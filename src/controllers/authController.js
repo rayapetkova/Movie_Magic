cb1785e83abc692ace03e05c09471e72c91ad5ba
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-22 13:01:44 +0300

    Display error messages on registration (if any)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,13 +9,25 @@ authController.get('/register', isGuest, (req, res) => {
 });
 
 authController.post('/register', isGuest, async (req, res) => {
-    const userData = req.body;
+    try {
+        const userData = req.body;
 
-    const token = await authService.register(userData);
+        const token = await authService.register(userData);
 
-    res.cookie('auth', token);
+        res.cookie('auth', token);
 
-    res.redirect('/');
+        res.redirect('/');
+    } catch (err) {
+        let errorMessage = '';
+
+        if (err.errors) {
+            errorMessage = Object.values(err.errors)[0].message;
+        } else {
+            errorMessage = err.message;
+        }
+
+        res.status(400).render('auth/register', { error: errorMessage });
+    }
 });
 
 authController.get('/login', isGuest, (req, res) => {
